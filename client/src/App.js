097636621b8d74861644031a7a0192a873fb6417
Nicole Remy <nicoleremy95@gmail.com>
2021-01-06 19:12:52 -0800

    Remove react presets

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -13,12 +12,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={getData}>Click Me For Data</button>
-        {data}
+       <h1>onramp</h1>
       </header>
     </div>
   );
