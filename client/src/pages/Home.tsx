fe4301eb94c2ece459f01bc24698173e00128778
Nicole Remy <nicoleremy95@gmail.com>
2021-01-07 17:34:49 -0800

    Npm i typescript @types, import * for react. Add appbar from materials ui as placeholder

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './home.css'
+import AppBar from '../components/Appbar/Appbar';
 
 export default function Home() {
     return (
         <div>
-            <h1>home page</h1>
+           <AppBar/>
         </div>
     )
 }
