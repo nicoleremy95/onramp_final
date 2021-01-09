baf5096b57d3ef9fd8b06121f2e5283111c9b9eb
Nicole Remy <nicoleremy95@gmail.com>
2021-01-09 12:25:42 -0800

    Links in footer working, Add styling

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,16 +3,34 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import {makeStyles} from '@material-ui/core/styles';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Link from '@material-ui/core/Link';
+
+import Icon from '@material-ui/core/Icon';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import {makeStyles, Theme} from '@material-ui/core/styles';
+import './footer.css'
 
 
-const useStyles = makeStyles(() =>({
+const useStyles = makeStyles((theme:Theme) =>({
     footer: {
         display: 'flex',
-        height: '100px !important',
+        // height: '150px !important',
         background: '#7a8a8a',
         bottom: 0,
         width: '1',
+        position:"relative"
+    },
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      margin: 'auto',
+      maxWidth: 500,
+    },
+    link:{
+      
     }
 }))
 export default function Footer() {
@@ -21,25 +39,36 @@ export default function Footer() {
       <React.Fragment>
         <CssBaseline />
         <div className ={classes.footer}>
-          <Typography component="div"  />
-          <Container>
-              <Grid item xs={3}>
-                <h2>suP?</h2>
+          <Typography component="div"/>
+          <Container className={classes.root}>
+            <Grid container spacing={2}>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <Typography gutterBottom variant="subtitle1" align="left">
+                    suP?
+                  </Typography>
+                  <Typography variant="body2" gutterBottom align="left">
+                    nicole remY
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" align="left">
+                    ID: 1030114
+                  </Typography>
+                </Grid>
+                <Grid item xs={1}>
+                  
+                  <Link color="inherit" href="https://www.linkedin.com/in/nicole-remy-190202166/" target="blank"><LinkedInIcon fontSize="large" /></Link>
+                  <Link color="inherit" href="https://github.com/nicoleremy95" target="blank"><GitHubIcon fontSize="large" /></Link>
+                </Grid>
+                <Grid item>
+                  <Typography className="more-talk" align="left" >
+                    more talK?
+                  </Typography>
+                </Grid> 
               </Grid>
-              <Grid item xs={3}>
+              <Grid item>
+                <Typography variant="subtitle1">@copyright 2021</Typography>
               </Grid>
-              <Grid item xs={3}>
-              </Grid>
-              <Grid item xs={3}>
-              </Grid>
-              <Grid item xs={6}>
-                <h1>more talK!</h1>
-              </Grid>
-              <Grid item xs={6}>
-              </Grid>
-              <Grid item xs={6}>
-                <h1>?</h1>
-              </Grid>
+            </Grid>
           </Container>
         </div>
       </React.Fragment>
