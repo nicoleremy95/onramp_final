322a56a7fd8f2d0c7fcbd2c6dfcc11f76216b660
Nicole Remy <nicoleremy95@gmail.com>
2021-01-09 06:44:51 -0800

    Add emoji modal for react button

--- client/src/components/News/News.tsx
+++ client/src/components/News/News.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import {useState, useEffect} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles, ThemeProvider, Theme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 import './news.css'
 
 //set up interface for props taken from parent component (Home.jsx)
@@ -14,12 +20,32 @@ interface Props {
     newsDB: object;
 }
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
     root: {
       alignItems: 'center',
       justifyContent: 'center',
-    }
-  });
+    },
+    textField:{
+      width: '55ch',
+      // padding: '5%'
+    },
+    container: {
+      margin: "5%"
+    },
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      backgroundColor: '#eee',
+      border: '2px solid #FFC0CB',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  })
+);
   
             // const newsId = news.data._id
             // const newsCreator = news.data.newsCreator;
@@ -27,6 +53,16 @@ const useStyles = makeStyles({
             
 //take props from parent Home.tsx
 export default function News({newsDB}: Props) : JSX.Element {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
     const classes = useStyles();
 
     const newsArr:object[] = [];
@@ -114,18 +150,49 @@ export default function News({newsDB}: Props) : JSX.Element {
                   fred
               </Typography>
               </CardContent>
-              <CardActions>
-                <Button size="small">comment</Button>
-                <Button size="small">react</Button>
+              <CardActions disableSpacing = {false}>
+                <IconButton aria-label="add to favorites">
+                  <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                  <ShareIcon />
+                </IconButton>
+                <Button size="small" onClick={handleOpen}>react</Button>
+                <Modal
+                  aria-labelledby="transition-modal-title"
+                  aria-describedby="transition-modal-description"
+                  className={classes.modal}
+                  open={open}
+                  onClose={handleClose}
+                  closeAfterTransition
+                  BackdropComponent={Backdrop}
+                  BackdropProps={{
+                    timeout: 500,
+                  }}
+                >
+                <Fade in={open}>
+                  <div className={classes.paper}>
+                    <Button size="small" className="News-cards-emoji">❤️️</Button>
+                    <Button size="small">😜️</Button>
+                    <Button size="small">😈</Button>
+                    <Button size="small">😍</Button>
+                    <Button size="small">😂</Button>
+                    <Button size="small">😊</Button>
+                  </div>
+                </Fade>
+              </Modal>
               </CardActions>
-              <TextField
-                id="filled-multiline-static"
-                label="comment"
-                // multiline
-                rows={4}
-                // defaultValue="hey there friend"
-                variant="filled"
-              />
+              <div
+                className="News-cards-comment"
+              >
+                <TextField
+                  id="filled-multiline-static"
+                  label="comment"
+                  variant="filled"
+                  className={classes.textField}
+                />
+              </div>
+              
          </Card>
         </div>
     )
