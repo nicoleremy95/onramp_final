dd3ecda3eb79125a3f674408b9a066ab3543fadf
Nicole Remy <nicoleremy95@gmail.com>
2021-01-09 09:48:15 -0800

    Add container footer to home page

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,3 +1,30 @@
 import * as React from 'react';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles(() =>({
+    container: {
+        width: '1'
+    },
+    footer: {
+        display: 'flex',
+        // flexDirection: 'column',
+        width: '100% !important',
+        height: '100px !important',
+        background: '#7a8a8a',
+        bottom: 0
+    }
+}))
+export default function Footer() {
+    const classes = useStyles();
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <div className ={classes.container}>
+          <Typography component="div" className={classes.footer} />
+        </div>
+      </React.Fragment>
+    );
+  }
