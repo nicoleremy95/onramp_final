fff85501a35faf46174f29047854f922e1693031
Nicole Remy <nicoleremy95@gmail.com>
2021-01-09 11:39:05 -0800

    Footer information started with grid

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,20 +1,18 @@
 import * as React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import {makeStyles} from '@material-ui/core/styles';
 
 
 const useStyles = makeStyles(() =>({
-    container: {
-        width: '1'
-    },
     footer: {
         display: 'flex',
-        // flexDirection: 'column',
-        width: '100% !important',
         height: '100px !important',
         background: '#7a8a8a',
-        bottom: 0
+        bottom: 0,
+        width: '1',
     }
 }))
 export default function Footer() {
@@ -22,8 +20,27 @@ export default function Footer() {
     return (
       <React.Fragment>
         <CssBaseline />
-        <div className ={classes.container}>
-          <Typography component="div" className={classes.footer} />
+        <div className ={classes.footer}>
+          <Typography component="div"  />
+          <Container>
+              <Grid item xs={3}>
+                <h2>suP?</h2>
+              </Grid>
+              <Grid item xs={3}>
+              </Grid>
+              <Grid item xs={3}>
+              </Grid>
+              <Grid item xs={3}>
+              </Grid>
+              <Grid item xs={6}>
+                <h1>more talK!</h1>
+              </Grid>
+              <Grid item xs={6}>
+              </Grid>
+              <Grid item xs={6}>
+                <h1>?</h1>
+              </Grid>
+          </Container>
         </div>
       </React.Fragment>
     );
